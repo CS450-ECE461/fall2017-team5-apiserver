ef07e2c42ae5a3c22fd8c10f50436bdb07b4a034
technetos <mrgould93@gmail.com>
2017-11-04 15:17:37 -0400

    added gatekeeper.config.js and added gatekeeper to blueprint app

---
+++ sojo-app/app/configs/gatekeeper.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  token: {
+    kind: 'jwt',
+    options: {
+      issuer: 'sojo-app',
+      algorithm : 'RS256',
+      secret: 'ssshhh'   // can replace with publicKey, privateKey properties
+    }
+  }
+};
