099ead506b30ac549fddfea04732ae612ac8417b
Kelsen Hazelwood <Kelsen@149-162-182-117.dhcp-in.iupui.edu>
2017-11-14 01:54:04 -0500

    Completed Unit model

---
+++ sojo-api/app/models/Unit.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongodb = require ('@onehilltech/blueprint-mongodb')
+;
+
+var schema = new mongodb.Schema({
+  apartment_complex_id : {type: ObjectId, required: true, trim: true},
+  lease_id             : {type: ObjectId, required: true, trim: true}
+});
+
+const COLLECTION_NAME = 'units';
+const MODEL_NAME = 'unit';
+
+module.exports = mongodb.resource (MODEL_NAME, schema, COLLECTION_NAME);
