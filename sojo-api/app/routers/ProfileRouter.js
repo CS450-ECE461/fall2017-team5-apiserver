f093571d0aa137c2c00cc0ab83338c605387d271
technetos <mrgould93@gmail.com>
2017-11-14 00:37:04 -0500

    removed unrealistic routes, added protection and denied get all

--- sojo-api/app/routers/ProfileRouter.js
+++ sojo-api/app/routers/ProfileRouter.js
@@ -1,17 +1,9 @@
 module.exports = {
-    '/user_profiles': {
-        resource: {
-            controller: 'ProfileController',
-
-            // optional list of operations to allow
-            allow: ['create', 'getOne', 'getAll', 'update', 'delete']
-        },
-        get: { action: 'ProfileController@showAll' },
-
-        '/:profileId': {
-            '/name': {
-                get: { action: 'ProfileController@showName' }
-            }
-        },
-    }
+  '/profiles': {
+    policy: 'gatekeeper.auth.bearer',
+    resource: {
+      controller: 'ProfileController',
+      deny: ['getAll'],
+    },
+  }
 };
