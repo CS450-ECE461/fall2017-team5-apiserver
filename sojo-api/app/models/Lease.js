d776daa2e83c9600b56d5affef01f584a5c25f67
Kelsen Hazelwood <Kelsen@149-162-182-117.dhcp-in.iupui.edu>
2017-11-14 15:06:18 -0500

    Updated to define types

--- sojo-api/app/models/Lease.js
+++ sojo-api/app/models/Lease.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const mongodb = require ('@onehilltech/blueprint-mongodb')
+const mongodb   = require ('@onehilltech/blueprint-mongodb')
+,     String    = mongodb.Schema.Types.String
+,     Number    = mongodb.Schema.Types.Number
+,     ObjectId  = mongodb.Schema.Types.ObjectId
 ;
 
 var schema = new mongodb.Schema ({
