a66ce2674f2342c999274be8aa8f19374630d859
Kelsen Hazelwood <Kelsen@149-162-182-117.dhcp-in.iupui.edu>
2017-11-14 01:10:05 -0500

    Created Lease model

---
+++ sojo-api/app/models/Lease.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongodb = require ('@onehilltech/blueprint-mongodb')
+;
+
+var schema = new mongodb.Schema({
+  user_id              : {type: Number, required: true, trim: true},
+  rent_amount          : {type: Number, required: true, trim: true},
+  start_date           : {type: Date, required: true, trim: true},
+  end_date             : {type: Date, required: true, trim: true},
+  apartment_complex_id : {type: ObjectId, required: true, trim: true}
+});
+
+const COLLECTION_NAME = 'leases';
+const MODEL_NAME = 'leases';
+
+module.exports = mongodb.resource (MODEL_NAME, schema, COLLECTION_NAME);
