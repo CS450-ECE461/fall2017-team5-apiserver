5dd681d9ab0c3833b57be0f2aa7b75a2b62875b5
technetos <mrgould93@gmail.com>
2017-11-14 11:35:57 -0500

    added lease_id to profile schema and fixed error from node about undefined types

--- sojo-api/app/models/Profile.js
+++ sojo-api/app/models/Profile.js
@@ -1,11 +1,15 @@
 'use strict';
 
-const mongodb = require ('@onehilltech/blueprint-mongodb')
+const mongodb   = require ('@onehilltech/blueprint-mongodb')
+,     String    = mongodb.Schema.Types.String
+,     Number    = mongodb.Schema.Types.Number
+,     ObjectId  = mongodb.Schema.Types.ObjectId
 ;
 
-var schema = new mongodb.Schema({
+var schema = new mongodb.Schema ({
   full_name: {type: String, required: true, trim: true},
-  phone    : {type: Number, required: true, trim: true}
+  phone    : {type: Number, required: true, trim: true},
+  lease_id : {type: ObjectId, required: true, trim: true},
 });
 
 const COLLECTION_NAME = 'profiles';
