d1810354252763b1f7c5efe9cbdf37f168d65a18
Kelsen Hazelwood <Kelsen@Kelsens-MacBook-Pro.local>
2017-11-14 10:59:54 -0500

    Changed address to street_address and added a space on line 6

--- sojo-api/app/models/Complex.js
+++ sojo-api/app/models/Complex.js
@@ -3,11 +3,11 @@
 const mongodb = require ('@onehilltech/blueprint-mongodb')
 ;
 
-var schema = new mongodb.Schema({
+var schema = new mongodb.Schema ({
   complex_name      : {type: String, required: true, trim: true},
   landlord_email    : {type: String, required: true, trim: true},
   maintenance_email : {type: String, required: true, trim: true},
-  address           : {type: String, required: true, trim: true}
+  street_address    : {type: String, required: true, trim: true}
 });
 
 const COLLECTION_NAME = 'complexes';
