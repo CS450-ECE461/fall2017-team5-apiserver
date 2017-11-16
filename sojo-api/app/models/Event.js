dd604421f34918971f481832311377e3ad526034
technetos <mrgould93@gmail.com>
2017-11-15 21:50:49 -0500

    implemented Event model

---
+++ sojo-api/app/models/Event.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongodb   = require ('@onehilltech/blueprint-mongodb')
+,     String    = mongodb.Schema.Types.String
+,     Number    = mongodb.Schema.Types.Number
+,     ObjectId  = mongodb.Schema.Types.ObjectId
+;
+
+var schema = new mongodb.Schema ({
+  name      : {type: String,      required: true,   trim: true},
+  date      : {type: Date,        required: true,   trim: true},
+  time      : {type: Date,        required: false,  trim: true},
+  site      : {type: String,      required: false,  trim: true},
+  attendees : {type: [ObjectId],  required: true,   trim: false}
+});
+
+const COLLECTION_NAME = 'leases';
+const MODEL_NAME      = 'lease';
+
+module.exports = mongodb.resource (MODEL_NAME, schema, COLLECTION_NAME);
