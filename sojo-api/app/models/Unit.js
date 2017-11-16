55cf46a8bb6ac826ac234c69329ba1eb501599d2
Rip Kahlon <rkahlon@imail.iu.edu>
2017-11-15 20:57:09 -0500

    Updated Unit Model

--- sojo-api/app/models/Unit.js
+++ sojo-api/app/models/Unit.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const mongodb       = require ('@onehilltech/blueprint-mongodb')
-    ,     String    = mongodb.Schema.Types.String
-    ,     Number    = mongodb.Schema.Types.Number
-    ,     ObjectId  = mongodb.Schema.Types.ObjectId
+const mongodb   = require ('@onehilltech/blueprint-mongodb')
+,     String    = mongodb.Schema.Types.String
+,     Number    = mongodb.Schema.Types.Number
+,     ObjectId  = mongodb.Schema.Types.ObjectId
 ;
 
-
 var schema = new mongodb.Schema ({
-  unit_index        : {type: String,   required: true, trim: true},
-  building_index    : {type: String,   required: true, trim: true},
-  maintenance_email : {type: String,   required: true, trim: true},
-  landlord_email    : {type: String,   required: true, trim: true},
-  account_id        : {type: ObjectId, required: true, trim: true}
+    unit_index        : {type: String,   required: true, trim: true},
+    building_index    : {type: String,   required: true, trim: true},
+    maintenance_email : {type: String,   required: true, trim: true},
+    landlord_email    : {type: String,   required: true, trim: true},
+    account_id        : {type: ObjectId, required: true, trim: true}
 });
 
 const COLLECTION_NAME = 'units';
