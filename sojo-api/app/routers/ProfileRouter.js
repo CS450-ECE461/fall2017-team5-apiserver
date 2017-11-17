55f019141d4fe41e293b951a81a0b46d761e0a5b
technetos <mrgould93@gmail.com>
2017-11-17 17:07:51 -0500

    Added routes under /profile/:id/foobar and method stubs on the controller for the route actions

--- sojo-api/app/routers/ProfileRouter.js
+++ sojo-api/app/routers/ProfileRouter.js
@@ -3,7 +3,22 @@ module.exports = {
     policy: 'gatekeeper.auth.bearer',
     resource: {
       controller: 'ProfileController',
-      deny: ['getAll'],
+      deny: ['getAll']
     },
+    '/:id': {
+      get: { action: 'ProfileController@Me' },
+      '/events': {
+        get: { action: 'ProfileController@getEvents' },
+        '/:id': {
+          get: { action: 'ProfileController@getEventById' },
+        }
+      },
+      '/utilities': {
+        get: { action: 'ProfileController@getUtilities' },
+        '/:id': {
+          get: { action: 'ProfileController@getUtilityById' },
+        }
+      }
+    }
   }
-};
+}
