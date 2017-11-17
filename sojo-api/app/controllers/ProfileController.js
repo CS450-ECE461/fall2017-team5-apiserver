338a56677e8a7503a29f577037c4872d116c399a
technetos <mrgould93@gmail.com>
2017-11-17 17:07:51 -0500

    Added routes under /profile/:id/foobar and method stubs on the controller for the route actions

--- sojo-api/app/controllers/ProfileController.js
+++ sojo-api/app/controllers/ProfileController.js
@@ -12,6 +12,57 @@ ProfileController.prototype.__defineGetter__ ('resourceId', function () {
   return 'profileId';
 });
 
+ProfileController.prototype.Me = (args) => {
+  return {
+    validate: (req, callback) => {
+      return callback (req.validationErrors (true));
+    },
+    sanitize: (req, callback) => {
+      return callback (null);
+    },
+    execute: (req, res, callback) => {
+      res.json ({ _id: 'ABCDEFH' });
+      return callback (null);
+    }
+  }
+}
+
+ProfileController.prototype.getEvents = (args) => {
+  return {
+    execute: (req, res, callback) => {
+      req.status (200).json ({});
+      return callback (null);
+    }
+  }
+}
+
+ProfileController.prototype.getEventById = (args) => {
+  return {
+    execute: (req, res, callback) => {
+      req.status (200).json ({});
+      return callback (null);
+    }
+  }
+}
+
+ProfileController.prototype.getUtilities = (args) => {
+  return {
+    execute: (req, res, callback) => {
+      req.status (200).json ({});
+      return callback (null);
+    }
+  }
+}
+
+ProfileController.prototype.getUtilityById = (args) => {
+  return {
+    execute: (req, res, callback) => {
+      req.status (200).json ({});
+      return callback (null);
+    }
+  }
+}
+
 blueprint.controller (ProfileController, ResourceController);
 
 module.exports = exports = ProfileController;
