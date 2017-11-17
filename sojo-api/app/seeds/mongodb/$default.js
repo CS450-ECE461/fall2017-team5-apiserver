be4c462b6366cf63c41a36be3450e02bc384cf05
technetos <mrgould93@gmail.com>
2017-11-17 13:58:26 -0500

    cleaned up default mongodb seed file dramatically

--- sojo-api/app/seeds/mongodb/$default.js
+++ sojo-api/app/seeds/mongodb/$default.js
@@ -12,48 +12,46 @@ const LOGIN_CLIENTS = {
   'ember-sojo-frontend': 0,
 };
 
-module.exports = {
-  clients: dab.times (1, function (i, opts, callback) {
-    var clientName = 'ember-sojo-frontend';
-    var client = {
-      _id: process.env.CLIENT_OBJECT_ID,
-      name: clientName,
-      client_secret: process.env.CLIENT_SECRET,
-      email: clientName + '@no-reply.com',
-      scope: scopes[i],
-      type: 'native',
-      recaptcha_secret: process.env.RECAPTCHA_SECRET
-    };
+module.exports = { 
+  clients: dab.times (1, (i, opts, callback) => {
+    let name = 'ember-sojo-frontend';
+    let client_id = process.env.CLIENT_OBJECT_ID;
+    let client_secret = process.env.CLIENT_SECRET;
+    let recaptcha_secret = process.env.RECAPTCHA_SECRET;
 
-    return callback (null, client);
+    return callback(null, {
+      _id: client_id,
+      name,
+      client_secret,
+      email: name + '@no-reply.com',
+      type: 'native',
+      recaptcha_secret
+    });
   }),
 
-  accounts: dab.times (1, function (i, opts, callback) {
+  accounts: dab.times (1, (i, opts, callback) => {
     var username = 'ember-sojo-frontend';
-    var account = {
+   
+    return callback(null, {
       created_by: dab.ref ('clients.0'),
-      username: username,
+      username,
       password: username,
       email: username + '@no-reply.com'
-    };
-  
-    return callback (null, account);
+    });
   }),
 
-  user_tokens: dab.map (dab.get ('accounts'), function (account, opts, callback) {
+  user_tokens: dab.map (dab.get ('accounts'), (account, opts, callback) => {
     const clientIndex = LOGIN_CLIENTS[account.username];
   
-    var model = {
+    return callback(null, {
       client: dab.get ('clients.0'),
       account: account._id,
       refresh_token: new ObjectId (),
       scope: scopes[clientIndex]
-    };
-  
-    return callback (null, model);
+    });
   }),
 
-  client_tokens: dab.map (dab.get ('clients'), function (client, opts, callback) {
+  client_tokens: dab.map (dab.get ('clients'), (client, opts, callback) => {
     return callback (null, {client: client._id});
   })
 };
