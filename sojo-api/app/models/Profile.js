bbd87c34bb6823c6040b657a79e12945eff52e85
technetos <mrgould93@gmail.com>
2017-11-18 13:04:52 -0500

    updated models/Profile with newy named sojo_event field

--- sojo-api/app/models/Profile.js
+++ sojo-api/app/models/Profile.js
@@ -7,10 +7,10 @@ const mongodb   = require ('@onehilltech/blueprint-mongodb')
 ;
 
 var schema = new mongodb.Schema ({
-  full_name  : {type: String,     required: true, trim: true  },
-  phone      : {type: Number,     required: true, trim: true  },
-  events     : {type: [ObjectId], required: true, trim: false },
-  account_id : {type: ObjectId,   required: true, trim: false }
+  full_name   : {type: String,     required: true, trim: true  },
+  phone       : {type: Number,     required: true, trim: true  },
+  sojo_events : {type: [ObjectId], required: true, trim: false },
+  account_id  : {type: ObjectId,   required: true, trim: false }
 });
 
 const COLLECTION_NAME = 'profiles';
