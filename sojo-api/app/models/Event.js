ba605d5cb70cca2241d619cf4bc226b11076dbd6
technetos <mrgould93@gmail.com>
2017-11-17 22:15:00 -0500

    commented isAttending

--- sojo-api/app/models/Event.js
+++ sojo-api/app/models/Event.js
@@ -14,6 +14,7 @@ var schema = new mongodb.Schema ({
   attendees : {type: [ObjectId],  required: true,   trim: false}
 });
 
+// Returns true if an account._id is in the attendees array
 schema.methods.isAttending = (account) => {
   return (this.attendees.filter((id) => account._id.str === id.str).length) > 0;
 }
