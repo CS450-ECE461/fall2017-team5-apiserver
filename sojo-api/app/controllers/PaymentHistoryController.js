cf10bc144660dc88b5096511f5a0e72d2649fa68
Rip Kahlon <ripkahlon@live.com>
2017-11-21 22:34:44 -0500

    PaymentHistoryController with inital getAll method

--- sojo-api/app/controllers/PaymentHistoryController.js
+++ sojo-api/app/controllers/PaymentHistoryController.js
@@ -14,6 +14,24 @@ PaymentHistoryController.prototype.__defineGetter__ ('resourceId', () => {
   return 'Id';
 });
 
+PaymentHistoryController.prototype.getAll = (args) => {
+  return {
+    execute: (req, res, callback) => {
+      PaymentHistory.find ({account_id: req.params.Id }, (err, payment_history) => {
+        if (err) {
+          res.status (404).json (err);
+        }
+        else if (!payment_history) {
+          res.status (404).send ('PaymentHistory not found');
+        }
+        else {
+          res.status (200).json (payment_history);
+        }
+      });
+    }
+  }
+}
+
 blueprint.controller (PaymentHistoryController, ResourceController);
 
 module.exports = exports = PaymentHistoryController;
