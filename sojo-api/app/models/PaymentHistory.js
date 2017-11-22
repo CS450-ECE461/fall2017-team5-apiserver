99880ffe3ef12da213066fb6357950f421b3413d
Rip Kahlon <ripkahlon@live.com>
2017-11-21 21:55:37 -0500

    Refixed styling

--- sojo-api/app/models/PaymentHistory.js
+++ sojo-api/app/models/PaymentHistory.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const mongodb       = require ('@onehilltech/blueprint-mongodb')
-    ,     String    = mongodb.Schema.Types.String
-    ,     Number    = mongodb.Schema.Types.Number
-    ,     ObjectId  = mongodb.Schema.Types.ObjectId
+const mongodb   = require ('@onehilltech/blueprint-mongodb')
+,     String    = mongodb.Schema.Types.String
+,     Number    = mongodb.Schema.Types.Number
+,     ObjectId  = mongodb.Schema.Types.ObjectId
 ;
 
 var schema = new mongodb.Schema ({
