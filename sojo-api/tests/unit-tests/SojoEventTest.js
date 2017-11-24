4c44244d5b3c2fe5016a471d9408525780fb9d80
Rip Kahlon <ripkahlon@live.com>
2017-11-24 12:08:41 -0500

    Added unit tests for getting SojoEvents and Units

--- sojo-api/tests/unit-tests/SojoEventTest.js
+++ sojo-api/tests/unit-tests/SojoEventTest.js
@@ -2,13 +2,13 @@ var blueprint = require ('@onehilltech/blueprint')
 , expect      = require ('chai').expect
 ;
 
-describe ('Utility', function () {
-  describe ('/utilities/000000000000000000000000', function () {
+describe ('SojoEvent', function () {
+  describe ('/events', function () {
     context ('GET', function () {
-      it ('should get a utility', function (done) {
+      it ('should get all events', function (done) {
         const accessToken = blueprint.app.seeds.$default.user_tokens[0].serializeSync ();
         blueprint.testing.request ()
-          .get ('/utilities/000000000000000000000000')
+          .get ('/events')
           .set('Authorization', 'Bearer ' + accessToken.access_token)
           .expect (200, done);
       });
--- sojo-api/tests/unit-tests/UnitTest.js
+++ sojo-api/tests/unit-tests/UnitTest.js
@@ -2,13 +2,13 @@ var blueprint = require ('@onehilltech/blueprint')
 , expect      = require ('chai').expect
 ;
 
-describe ('Utility', function () {
-  describe ('/utilities/000000000000000000000000', function () {
+describe ('Unit', function () {
+  describe ('/units/000000000000000000000000', function () {
     context ('GET', function () {
-      it ('should get a utility', function (done) {
+      it ('should get a unit', function (done) {
         const accessToken = blueprint.app.seeds.$default.user_tokens[0].serializeSync ();
         blueprint.testing.request ()
-          .get ('/utilities/000000000000000000000000')
+          .get ('/units/000000000000000000000000')
           .set('Authorization', 'Bearer ' + accessToken.access_token)
           .expect (200, done);
       });
