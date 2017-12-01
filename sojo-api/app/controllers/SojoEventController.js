38fde19edb3556c73c62d6da0c72debe3cbab2d4
Rip Kahlon <ripkahlon@live.com>
2017-12-01 16:59:52 -0500

    Updated SojoController to return all non-private events on getAll

--- sojo-api/app/controllers/SojoEventController.js
+++ sojo-api/app/controllers/SojoEventController.js
@@ -17,12 +17,12 @@ SojoEventController.prototype.__defineGetter__ ('resourceId', () => {
 SojoEventController.prototype.getAll = (args) => {
   return {
     execute: (req, res, callback) => {
-      SojoEvent.find ({ account_id: req.params.Id }, (err, sojo_event) => {
+      SojoEvent.find ({ is_private: false }, (err, sojo_event) => {
         if (err) {
           res.status (400).json (err);
         }
         else if (!sojo_event) {
-          res.status (404).send ('SojoEvent not found');
+          res.status (404).json ('SojoEvent not found');
         }
         else {
           res.status (200).json (sojo_event);
@@ -32,6 +32,10 @@ SojoEventController.prototype.getAll = (args) => {
   }
 }
 
+
+
 blueprint.controller (SojoEventController, ResourceController);
 
 module.exports = exports = SojoEventController;
+
+
