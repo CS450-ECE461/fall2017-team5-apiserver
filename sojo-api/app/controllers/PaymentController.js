f9dd620c369c915aec69d597d9b036effa66d0a1
technetos <mrgould93@gmail.com>
2017-12-01 23:56:50 -0500

    updated Payment controller and router to support

--- sojo-api/app/controllers/PaymentController.js
+++ sojo-api/app/controllers/PaymentController.js
@@ -14,10 +14,104 @@ PaymentController.prototype.__defineGetter__ ('resourceId', () => {
   return 'Id';
 });
 
+PaymentController.prototype.__defineGetter__ ('utilityId', () => {
+  return 'UtilityId';
+});
+
+PaymentController.prototype.__defineGetter__ ('leaseId', () => {
+  return 'LeaseId';
+});
+
 PaymentController.prototype.get = (args) => {
   return {
     execute: (req, res, callback) => {
       Payment.find ({account_id: req.params.Id }, (err, payment) => {
+        if (err) {
+          res.status (400).json (err);
+        }
+        else if (!payment) {
+          res.status (404).send ('Payment not found');
+        }
+        else {
+          res.status (200).json (payment);
+        }
+      });
+    }
+  }
+}
+
+PaymentController.prototype.getAllRentPayments = () => {
+  return {
+    execute: (req, res, callback) => {
+      Payment.find ({
+        account_id: req.params.Id,
+        payment_type: 'lease'
+      }, (err, payment) => {
+        if (err) {
+          res.status (400).json (err);
+        }
+        else if (!payment) {
+          res.status (404).send ('Payment not found');
+        }
+        else {
+          res.status (200).json (payment);
+        }
+      });
+    }
+  }
+}
+
+PaymentController.prototype.getOneRentPayment = () => {
+  return {
+    execute: (req, res, callback) => {
+      Payment.findOne ({
+        account_id: req.params.Id,
+        payment_type: 'lease',
+        payment_object: req.params.LeaseId
+      }, (err, payment) => {
+        if (err) {
+          res.status (400).json (err);
+        }
+        else if (!payment) {
+          res.status (404).send ('Payment not found');
+        }
+        else {
+          res.status (200).json (payment);
+        }
+      });
+    }
+  }
+}
+
+PaymentController.prototype.getAllUtilityPayments = () => {
+  return {
+    execute: (req, res, callback) => {
+      Payment.find ({
+        account_id: req.params.Id,
+        payment_type: 'utility'
+      }, (err, payment) => {
+        if (err) {
+          res.status (400).json (err);
+        }
+        else if (!payment) {
+          res.status (404).send ('Payment not found');
+        }
+        else {
+          res.status (200).json (payment);
+        }
+      });
+    }
+  }
+}
+
+PaymentController.prototype.getOneUtilityPayment = () => {
+  return {
+    execute: (req, res, callback) => {
+      Payment.find ({
+        account_id: req.params.Id,
+        payment_type: 'utility',
+        payment_object: req.params.UtilityId
+      }, (err, payment) => {
         if (err) {
           res.status (400).json (err);
         }
