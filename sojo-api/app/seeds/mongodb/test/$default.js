90c69862b8a4366d6a5c94ae4e6d9e16bdc44974
Rip Kahlon <ripkahlon@live.com>
2017-12-04 17:03:54 -0500

    Added completed events test, and profiles test

--- sojo-api/app/seeds/mongodb/test/$default.js
+++ sojo-api/app/seeds/mongodb/test/$default.js
@@ -76,11 +76,11 @@ module.exports = {
 
     if (is_even) {
       return callback (null, {
-        name: 'Albino Peach Eating Contest',
+        name: 'Bathroom broke',
         date: new Date(),
         start_time: new Date(2017, 2, 2, 12),
         end_time: new Date(2017, 2, 2, 4),
-        description: 'Cookout Garden',
+        description: 'Not working',
         type: 'Maintenance request',
         account_id: (dab.ref ('accounts.0'))
       });
--- sojo-api/tests/unit-tests/SojoEventTest.js
+++ sojo-api/tests/unit-tests/SojoEventTest.js
@@ -8,12 +8,25 @@ describe ('SojoEvent', function () {
   describe ('/events/', function () {
     context ('GET', function () {
       it ('should get all events that are public', function (done) {
+        const event0 = blueprint.app.seeds.$default.sojo_events[1];
         const accessToken = blueprint.app.seeds.$default.user_tokens[0].serializeSync ();
         blueprint.testing.request ()
           .get ('/events')
           .set ('Authorization', 'Bearer ' + accessToken.access_token)
-          .expect (200, mongodb.lean ({
-          }), done);
+          .expect (200, mongodb.lean (
+            [
+              {
+                _id: event0._id,
+                name: event0.name,
+                date: event0.date,
+                start_time: event0.start_time,
+                end_time: event0.end_time,
+                description: event0.description,
+                type: event0.type,
+                __v: event0.__v
+              }
+            ]
+          ), done);
       });
     });
   });
@@ -25,8 +38,19 @@ describe ('SojoEvent', function () {
         blueprint.testing.request ()
           .get ('/events/' + event0._id)
           .set ('Authorization', 'Bearer ' + accessToken.access_token)
-          .expect (200, mongodb.lean ({
-          }), done);
+          .expect (200, mongodb.lean (
+              {
+                _id: event0._id,
+                name: event0.name,
+                date: event0.date,
+                start_time: event0.start_time,
+                end_time: event0.end_time,
+                description: event0.description,
+                type: event0.type,
+                account_id: event0.account_id,
+                __v: event0.__v
+              }
+          ), done);
       });
     });
   });
@@ -34,7 +58,6 @@ describe ('SojoEvent', function () {
     context ('GET', function () {
       it ('should get all private events for an account', (done) => {
         const event0 = blueprint.app.seeds.$default.sojo_events[0];
-
         const accessToken = blueprint.app.seeds.$default.user_tokens[0].serializeSync ();
         const account0 = blueprint.app.seeds.$default.accounts[0];
         blueprint.testing.request ()
