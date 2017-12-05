a902490c3ff610d63f6574dc034e27ce8249d299
technetos <mrgould93@gmail.com>
2017-12-05 01:56:17 -0500

    added cors to confirm route

--- sojo-api/app/routers/ActivationRouter.js
+++ sojo-api/app/routers/ActivationRouter.js
@@ -1,5 +1,14 @@
+const cors = require ('cors')
+;
+
+const corsOptions = {
+    origin: "*",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'SEARCH']
+};
+
 module.exports = {
   '/confirm': {
+    use: cors (corsOptions),
     policy: 'gatekeeper.auth.bearer',
     post: { action: 'ActivationController' }
   }
