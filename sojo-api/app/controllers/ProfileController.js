cc135211a83e9f4ef1b300df977008dbc0ada721
technetos <mrgould93@gmail.com>
2017-12-05 17:06:34 -0500

    implemented put route for profile

--- sojo-api/app/controllers/ProfileController.js
+++ sojo-api/app/controllers/ProfileController.js
@@ -32,6 +32,37 @@ ProfileController.prototype.get = (args) => {
   }
 }
 
+ProfileController.prototype.update = (args) => {
+  return {
+    validate: (req, callback) => {
+      req.checkBody ('profile.full_name', 'Missing full_name').notEmpty ();
+      req.checkBody ('profile.phone', 'Missing phone').notEmpty ();
+      req.checkBody ('profile.has_bill_pay_setup', 'Missing has_bill_pay_setup').notEmpty ();
+      req.checkBody ('profile.has_signed_lease', 'Missing has_signed_lease').notEmpty ();
+      req.checkBody ('profile.electric_utility', 'Missing electric_utility').notEmpty ();
+      req.checkBody ('profile.cable_utility', 'Missing cable_utility').notEmpty ();
+      return callback (req.validationErrors (true));
+    },
+
+    execute: (req, res, callback) => {
+      Profile.findOne ({ account_id: req.params.Id }, (err, profile) => {
+        profile.full_name = req.body.profile.full_name;
+        profile.phone = req.body.profile.phone;
+        profile.has_bill_pay_setup = req.body.profile.phone;
+        profile.has_signed_lease = req.body.profile.has_signed_lease;
+        if (req.body.profile.account_picture_url !== undefined) {
+          profile.account_picture_url = req.body.profile.account_picture_url;
+        }
+        profile.electric_utility = req.body.profile.electric_utility;
+        profile.cable_utility = req.body.profile.cable_utility;
+        profile.save ()
+          res.status (200).json ({});
+        });
+      });
+    }
+  }
+}
+
 blueprint.controller (ProfileController, ResourceController);
 
 module.exports = exports = ProfileController;
