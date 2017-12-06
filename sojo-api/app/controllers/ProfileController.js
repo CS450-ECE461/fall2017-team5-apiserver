eedd49e097f4d65a355c0668cf47984765eee0d4
technetos <mrgould93@gmail.com>
2017-12-05 20:46:49 -0500

    actualized demo data

--- sojo-api/app/controllers/ProfileController.js
+++ sojo-api/app/controllers/ProfileController.js
@@ -34,34 +34,38 @@ ProfileController.prototype.get = (args) => {
 
 ProfileController.prototype.update = (args) => {
   return {
-    validate: (req, callback) => {
-      req.checkBody ('profile.full_name', 'Missing full_name').notEmpty ();
-      req.checkBody ('profile.phone', 'Missing phone').notEmpty ();
-      req.checkBody ('profile.has_bill_pay_setup', 'Missing has_bill_pay_setup').notEmpty ();
-      req.checkBody ('profile.has_signed_lease', 'Missing has_signed_lease').notEmpty ();
-      req.checkBody ('profile.electric_utility', 'Missing electric_utility').notEmpty ();
-      req.checkBody ('profile.cable_utility', 'Missing cable_utility').notEmpty ();
-      return callback (req.validationErrors (true));
-    },
-
     execute: (req, res, callback) => {
       Profile.findOne ({ account_id: req.params.Id }, (err, profile) => {
-        profile.full_name = req.body.profile.full_name;
-        profile.phone = req.body.profile.phone;
-        profile.has_bill_pay_setup = req.body.profile.phone;
-        profile.has_signed_lease = req.body.profile.has_signed_lease;
-        if (req.body.profile.account_picture_url !== undefined) {
-          profile.account_picture_url = req.body.profile.account_picture_url;
+        if (!err && profile) {
+          if (req.body.profile.full_name !== undefined || null) {
+            profile.full_name = req.body.profile.full_name;
+          }
+          if (req.body.profile.phone !== undefined || null) {
+            profile.phone = req.body.profile.phone;
+          }
+          if (req.body.profile.has_bill_pay_setup !== undefined || null) {
+            profile.has_bill_pay_setup = req.body.profile.has_bill_pay_setup;
+          }
+          if (req.body.profile.has_signed_lease !== undefined || null) {
+            profile.has_signed_lease = req.body.profile.has_signed_lease;
+          }
+          if (req.body.profile.account_picture_url !== undefined || null) {
+            profile.account_picture_url = req.body.profile.account_picture_url;
+          }
+          if (req.body.profile.electric_utility !== undefined || null) {
+            profile.electric_utility = req.body.profile.electric_utility;
+          }
+          if (req.body.profile.cable_utility !== undefined || null) {
+            profile.cable_utility = req.body.profile.cable_utility;
+          }
+          profile.save ()
         }
-        profile.electric_utility = req.body.profile.electric_utility;
-        profile.cable_utility = req.body.profile.cable_utility;
-        profile.save ()
-          res.status (200).json ({});
-        });
+        res.status (200).json ({});
       });
     }
   }
 }
+
 
 blueprint.controller (ProfileController, ResourceController);
 
